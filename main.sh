8e6804c12da20800c3ee47fa2b784efc52b09b80
joe-broder15 <joe.broder@berkeley.edu>
2025-04-15 23:23:23 -0400

    removed ashby from main script and changed dork

--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
 python parse_company_portals.py --software greenhouse
 python parse_company_portals.py --software lever
-python parse_company_portals.py --software ashby
 python scrape_portals.py --software greenhouse
 python scrape_portals.py --software lever
-python scrape_portals.py --software ashby
 python merge_csv.py
